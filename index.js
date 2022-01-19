863e8265746812fde16fa22af69258e4a832059c
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2022-01-19 14:59:09 +0700

    added variable scope

--- index.js
+++ index.js
@@ -25,3 +25,35 @@ function murid(nama, kelas) {
 
 let naufal = murid("Naufal", "10 RPL B")
 naufal()
+
+//Variable
+/*
+ *	Var
+ *		- Bersifat mutable artinya bisa merubah value atau nilainya
+ *		- Memiliki dua function scope, global dan local scope
+ */
+
+ /*
+	*	Let
+	* 	- Global, local, block Scope
+	* 	- Mutable
+  */
+
+	/*
+	 * Const 
+	 * 	- Global, local, block scope
+	 *  - Immutable
+	 */
+
+const jeneng = 'Dajang'
+
+function dajangoke(dajang) {
+	if (dajang) {
+		console.log(jeneng)
+		const jenengDajang = 'altap'
+	}
+	console.log(jenengDajang)
+
+}
+
+dajangoke(true)
