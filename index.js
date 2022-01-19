e55c79eb093db1c54952b6bfb503a252dce3729c
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2022-01-19 13:49:42 +0700

    added closure

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 //Global Scope Ex
-const nama = "Naufal"
+// const nama = "Naufal"
 
-const printName = () => {
-	console.log(nama)
+// const printName = () => {
+// 	console.log(nama)
+// }
+
+// printName()
+
+//Closure
+// function hello(nama, julukan) {
+// 	var a = "hello " + nama + ' ' + julukan;
+
+// 	return function() {
+// 		console.log(a)
+// 	}
+// }
+
+// let john = hello("John", "Si tuakng ngutang")
+// john()
+
+function murid(nama, kelas) {
+	var data = `Nama: ${nama} \nKelas: ${kelas}`
+
+	function displayname() {
+		console.log(data)
+	}
+
+	return displayname
 }
 
-printName()
+let naufal = murid("Naufal", "10 RPL B")
+naufal()
